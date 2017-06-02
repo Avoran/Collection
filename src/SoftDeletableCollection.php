b5f10155ad483c9f4c10695e4d080cf66bacad9e
Pieter Verloop <p.verloop@plein.nl>
2017-06-02 15:17:46 +0200

    use Doctrine\Common\Collections\Collection as DoctrineCollection

--- src/SoftDeletableCollection.php
+++ src/SoftDeletableCollection.php
@@ -4,21 +4,21 @@ namespace Avoran\Collection;
 
 use Doctrine\Common\Collections\AbstractLazyCollection;
 use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\Collection as DoctrineCollection;
 
 class SoftDeletableCollection extends AbstractLazyCollection
 {
     private $includeSoftDeleted;
     private $completeCollection;
 
-    private function __construct(Collection $collection, $includeSoftDeleted)
+    private function __construct(DoctrineCollection $collection, $includeSoftDeleted)
     {
         $this->includeSoftDeleted = $includeSoftDeleted;
         $this->completeCollection = $collection;
     }
 
     public static function fromArray(array $array) { return new static(new ArrayCollection($array), false); }
-    public static function fromCollection(Collection $collection) { return new static($collection, false); }
+    public static function fromCollection(DoctrineCollection $collection) { return new static($collection, false); }
 
     public function includeSoftDeleted() { return new static($this->completeCollection, true); }
 
